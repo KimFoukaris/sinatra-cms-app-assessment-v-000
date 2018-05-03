91f5798efd4bb9bc8f76337cb3807b0d0113b667
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-03 14:08:15 +0000

    automatically backed up by learn

--- volunteer-management-mvc/app/controllers/users_controller.rb
+++ volunteer-management-mvc/app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    if !session[:user_id]
+      erb :'users/new'
+    else
+      redirect to '/volunteer'
+    end
+  end
 end
